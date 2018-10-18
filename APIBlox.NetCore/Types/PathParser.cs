c94eddb8c82cf2ec714f063cd275677804a64225
Slacquer <slacquer2018@gmail.com>
2018-10-18 13:11:50 -0500

    cleaner and parser works pretty well (for what i want anyways).

---
+++ APIBlox.NetCore/Types/PathParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using APIBlox.NetCore.Extensions;
+
+namespace APIBlox.NetCore.Types
+{
+    /// <summary>
+    ///     Class PathParser.
+    /// </summary>
+    public class PathParser
+    {
+        private readonly string[] _pathParts;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PathParser" /> class.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        public PathParser(string path)
+        {
+            _pathParts = path.Split(new[] { "**" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        ///     Gets a list of directory info objects using ** pattern matching.
+        /// </summary>
+        /// <param name="filterAction">Filter results while being added.</param>
+        /// <returns>IEnumerable&lt;DirectoryInfo&gt;.</returns>
+        public IEnumerable<DirectoryInfo> GetDirectories(Func<string, bool> filterAction = null)
+        {
+            if (_pathParts.Length == 1)
+                return new[] { new DirectoryInfo(_pathParts[0]) };
+
+            var paths = new List<string>();
+
+            var root = new DirectoryInfo(_pathParts[0]).FullName;
+            paths.AddRange(Directory.GetDirectories(root, "*", SearchOption.AllDirectories)
+                .Where(d =>
+                    _pathParts.Any(p => d.EndsWithEx(p))
+                    && !paths.Any(di => di.EqualsEx(d))
+                    && (filterAction?.Invoke(d) ?? true)
+                )
+            );
+
+            return paths.Distinct().Select(p => new DirectoryInfo(p));
+        }
+    }
+}
---
+++ SlnTests/APIBlox.NetCore/PathParserTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using APIBlox.NetCore.Extensions;
+using APIBlox.NetCore.Types;
+using Xunit;
+
+namespace SlnTests.APIBlox.NetCore
+{
+    public class PathParserTests : IDisposable
+    {
+        public PathParserTests()
+        {
+            var path = Path.GetTempPath();
+            var root = Path.Combine(path, @"a\b\c\d\e\");
+
+            _paths.Add(root);
+            _paths.Add(Path.Combine(path, root, @"f\g\findMe"));
+            _paths.Add(Path.Combine(path, root, @"h\i\findMe"));
+            _paths.Add(Path.Combine(path, root, @"h\bin\findMe"));
+
+            for (var i = 0; i < 10; i++)
+                _paths.Add(Path.Combine(root, i.ToString()));
+
+            _paths.Add(Path.Combine(_paths.Last(), "findMeToo"));
+
+            foreach (var p in _paths)
+            {
+                if (!Directory.Exists(p))
+                    Directory.CreateDirectory(p);
+            }
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(_paths[0], true);
+        }
+
+        private readonly List<string> _paths = new List<string>();
+
+        [Fact]
+        public void ShouldHavePathsFromSingleDoubleAsterisks()
+        {
+            var parser = new PathParser(_paths[1].Replace(@"\g", @"\**")).GetDirectories().ToList();
+
+            Assert.NotNull(parser);
+            Assert.True(parser.Count > 0);
+
+            var hasRelease = parser.Any(d => d.FullName.EndsWithEx("findme"));
+
+            Assert.True(hasRelease);
+        }
+
+        [Fact]
+        public void ShouldHavePathsFromSingleDoubleAsterisksOnlyBin()
+        {
+            var parser = new PathParser(_paths[3].Replace(@"\a", @"\**")).GetDirectories().ToList();
+
+            Assert.NotNull(parser);
+            Assert.True(parser.Count > 0);
+
+            var hasRelease = parser.Any(d => d.FullName.EndsWithEx("findme"));
+
+            Assert.True(hasRelease);
+        }
+
+        [Fact]
+        public void ShouldHaveSinglePathAsNoAsterisksWereProvided()
+        {
+            var parser = new PathParser(_paths[2]).GetDirectories().ToList();
+
+            Assert.NotNull(parser);
+            Assert.True(parser.Count == 1);
+        }
+    }
+}
