7e77586b18dbee396076d6777f96a2de18999d29
Slacquer <slacquer2018@gmail.com>
2019-03-28 16:08:38 -0500

    wip

--- APIBlox.AspNetCore.CommandsQueriesControllersOhMy/Controllers/DynamicQueryAllController.cs
+++ APIBlox.AspNetCore.CommandsQueriesControllersOhMy/Controllers/DynamicQueryAllController.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using APIBlox.AspNetCore.ActionResults;
 using APIBlox.AspNetCore.Contracts;
 using APIBlox.AspNetCore.Types;
+using APIBlox.NetCore.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace APIBlox.AspNetCore.Controllers
 {
@@ -55,11 +58,11 @@ namespace APIBlox.AspNetCore.Controllers
             if (ret.HasErrors)
                 return new ProblemResult(ret.Error);
 
-            return ret.Result is null ? NoContent() : (IActionResult) Ok(ret.Result);
+            return ret.Result is null ? NoContent() : (IActionResult)Ok(ret.Result);
         }
     }
 
-        /// <summary>
+    /// <summary>
     ///     Class DynamicQueryAllController.
     /// </summary>
     /// <typeparam name="TRequest">The type of the t request.</typeparam>
@@ -104,7 +107,74 @@ namespace APIBlox.AspNetCore.Controllers
             if (ret.HasErrors)
                 return new ProblemResult(ret.Error);
 
-            return ret.Result is null ? NoContent() : (IActionResult) Ok(ret.Result);
+            return ret.Result is null ? NoContent() : (IActionResult)Ok(ret.Result);
+        }
+    }
+
+        /// <summary>
+    ///     Class DynamicQueryAllController. This class cannot be inherited.
+    /// Implements the <see cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    /// Implements the <see cref="APIBlox.AspNetCore.Contracts.IDynamicController{TRequest}" />
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the t request.</typeparam>
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class DynamicQueryAllController<TRequest> : ControllerBase,
+        IDynamicController<TRequest>
+    {
+        private readonly IQueryHandler<TRequest, HandlerResponse> _getAllHandler;
+        private readonly ILogger _log;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="DynamicQueryAllController{TRequest}"/> class.
+        /// </summary>
+        /// <param name="loggerFactory">The logger factory.</param>
+        /// <param name="getAllHandler">The get all handler.</param>
+        public DynamicQueryAllController(
+            ILoggerFactory loggerFactory,
+            IQueryHandler<TRequest, HandlerResponse> getAllHandler)
+        {
+            _log = loggerFactory.CreateLogger(GetType());
+            _getAllHandler = getAllHandler;
+        }
+
+        /// <summary>
+        ///     Action for getting a collection of resources.
+        ///     <para>
+        ///         Responses: 200, 204, 401, 403
+        ///     </para>
+        /// </summary>
+        /// <param name="request">TRequest input parameter(s)</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Task&lt;IActionResult&gt;.</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<IResource>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<IActionResult> GetAll([FromRoute] TRequest request, CancellationToken cancellationToken)
+        {
+            var ret = await _getAllHandler.HandleAsync(request, cancellationToken).ConfigureAwait(false);
+
+            if (ret.HasErrors)
+                return new ProblemResult(ret.Error);
+
+            return ret.Result is null ? NoContent() : (IActionResult)Ok(ValidateEnumerable(ret.Result));
+        }
+
+        private object ValidateEnumerable(object result)
+        {
+            var t = result.GetType();
+
+            var enumerable = t.IsAssignableTo(typeof(IEnumerable)) && !t.IsAssignableTo(typeof(string));
+
+            if (!enumerable)
+                _log.LogWarning(() =>
+                    $"GETALL controller being used at route '{Request.Path.Value}' with a result ({t.Name}) " +
+                    "that does not appear to be enumerable.  Is this correct?"
+                );
+
+            return result;
         }
     }
 }
--- APIBlox.AspNetCore.CommandsQueriesControllersOhMy/Controllers/DynamicQueryByController.cs
+++ APIBlox.AspNetCore.CommandsQueriesControllersOhMy/Controllers/DynamicQueryByController.cs
@@ -64,7 +64,7 @@ namespace APIBlox.AspNetCore.Controllers
         }
     }
 
-        /// <summary>
+    /// <summary>
     ///     Class DynamicQueryByController.
     /// </summary>
     /// <typeparam name="TRequest">The type of the t request.</typeparam>
@@ -76,6 +76,58 @@ namespace APIBlox.AspNetCore.Controllers
     public sealed class DynamicQueryByController<TRequest, TResponse> : ControllerBase,
         IDynamicController<TRequest, TResponse>
         where TResponse : IResource
+    {
+        private readonly IQueryHandler<TRequest, HandlerResponse> _getByHandler;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="DynamicQueryByController{TRequest, TResponse}" /> class.
+        /// </summary>
+        /// <param name="getByHandler">The get by handler.</param>
+        public DynamicQueryByController(IQueryHandler<TRequest, HandlerResponse> getByHandler)
+        {
+            _getByHandler = getByHandler;
+        }
+
+        /// <summary>
+        ///     Action for getting a resource.
+        ///     <para>
+        ///         Responses: 200, 401, 403, 404
+        ///     </para>
+        /// </summary>
+        /// <param name="request">TRequest input parameter(s)</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Task&lt;IActionResult&gt;.</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IResource), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get([FromRoute] TRequest request, CancellationToken cancellationToken)
+        {
+            var ret = await _getByHandler.HandleAsync(request, cancellationToken).ConfigureAwait(false);
+
+            if (ret.HasErrors)
+                return new ProblemResult(ret.Error);
+
+            if (ret.Result is null)
+                throw new NullReferenceException(
+                    "When responding to a GET you must either set an error or pass a result!"
+                );
+
+            return Ok(ret.Result);
+        }
+    }
+
+    /// <summary>
+    ///     Class DynamicQueryByController. This class cannot be inherited.
+    /// Implements the <see cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    /// Implements the <see cref="APIBlox.AspNetCore.Contracts.IDynamicController{TRequest}" />
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the t request.</typeparam>
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class DynamicQueryByController<TRequest> : ControllerBase,
+        IDynamicController<TRequest>
     {
         private readonly IQueryHandler<TRequest, HandlerResponse> _getByHandler;
 
