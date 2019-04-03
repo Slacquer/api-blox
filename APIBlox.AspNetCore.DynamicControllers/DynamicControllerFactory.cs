a61d6f444b25d701ad6a910090c66f18fedf267d
Slacquer <slacquer2018@gmail.com>
2019-04-03 18:32:37 -0500

    wip

--- APIBlox.AspNetCore.DynamicControllers/DynamicControllerFactory.cs
+++ APIBlox.AspNetCore.DynamicControllers/DynamicControllerFactory.cs
@@ -104,8 +104,8 @@ namespace APIBlox.AspNetCore
                 Directory.CreateDirectory(assemblyOutputPath);
 
             var fi = EmitToFile(assemblyOutputPath, useCache, templates);
-
-            return fi.Exists ? Assembly.LoadFile(fi.FullName) : null;
+            
+            return (fi is null || fi.Exists) ? Assembly.LoadFile(fi.FullName) : null;
         }
 
         /// <summary>
@@ -508,7 +508,7 @@ namespace APIBlox.AspNetCore
 
             if (useCache && OutputsCheck(dll, pdb, xml))
                 return dll;
-            
+
             var csOptions = GetSyntaxTree(templates, out var csSyntaxTree);
             var compilation = CSharpCompilation.Create(_assemblyName, csSyntaxTree, GetReferences(), csOptions);
 
